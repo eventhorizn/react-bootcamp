12ab07418e7157f258b2188bcb54b798d7c3a5b7
Gary Hake <Gary.Hake@waystar.com>
2021-03-10 09:48:17 -0500

    fixed style issues

--- 03-style-react-components/src/App.js
+++ 03-style-react-components/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 //import Radium, { StyleRoot } from 'radium';
-//import styled from 'styled-components';
+import styled from 'styled-components';
 import Person from './Person/Person';
 
-// const StyledButton = styled.button`
-// 	background-color: ${(props) => (props.alt ? 'red' : 'green')};
-// 	color: white;
-// 	font: inherit;
-// 	border: 1px solid blue;
-// 	padding: 8px;
-// 	cursor: pointer;
-// 	&:hover {
-// 		background-color: ${(props) => (props.alt ? 'salmon' : 'lightgreen')};
-// 		color: black;
-// 	}
-// `;
+const StyledButton = styled.button`
+	background-color: ${(props) => (props.alt ? 'red' : 'green')};
+	color: white;
+	font: inherit;
+	border: 1px solid blue;
+	padding: 8px;
+	cursor: pointer;
+	&:hover {
+		background-color: ${(props) => (props.alt ? 'salmon' : 'lightgreen')};
+		color: black;
+	}
+`;
 
 class App extends Component {
 	state = {
@@ -57,18 +57,18 @@ class App extends Component {
 	};
 
 	render() {
-		// const style = {
-		// 	backgroundColor: 'green',
-		// 	color: 'white',
-		// 	font: 'inherit',
-		// 	border: '1px solid blue',
-		// 	padding: '8px',
-		// 	cursor: 'pointer',
-		// 	':hover': {
-		// 		backgroundColor: 'lightgreen',
-		// 		color: 'black',
-		// 	},
-		// };
+		const style = {
+			backgroundColor: 'green',
+			color: 'white',
+			font: 'inherit',
+			border: '1px solid blue',
+			padding: '8px',
+			cursor: 'pointer',
+			':hover': {
+				backgroundColor: 'lightgreen',
+				color: 'black',
+			},
+		};
 
 		let persons = null;
 
@@ -110,17 +110,15 @@ class App extends Component {
 			<div className="App">
 				<h1>Hi, I'm a react app</h1>
 				<p className={classes.join(' ')}>This is really working!</p>
-				{/* <StyledButton
+				<StyledButton
 					alt={this.state.showPersons}
 					onClick={this.togglePersonsHandler}
 				>
 					Toggle Persons
-				</StyledButton> */}
+				</StyledButton>
 				{/* <button style={style} onClick={this.togglePersonsHandler}>
 					Toggle Persons
 				</button> */}
-
-				<button>Toggle Persons</button>
 
 				{persons}
 			</div>
