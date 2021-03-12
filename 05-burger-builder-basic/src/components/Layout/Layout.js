515bfaef069dba24c55205b6e1a6beb31fa45037
Gary Hake <Gary.Hake@waystar.com>
2021-03-12 09:03:43 -0500

    implemented mobile side drawer

--- 05-burger-builder-basic/src/components/Layout/Layout.js
+++ 05-burger-builder-basic/src/components/Layout/Layout.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import styles from './Layout.module.css';
 
-const layout = (props) => (
-	// Adjacent elements
-	<Aux>
-		<Toolbar />
-		<main className={styles.content}>{props.children}</main>
-	</Aux>
-);
+class Layout extends Component {
+	state = {
+		showSideDrawer: false,
+	};
 
-export default layout;
+	sideDrawerToggleHandler = () => {
+		this.setState({ showSideDrawer: !this.state.showSideDrawer });
+	};
+
+	sideDrawerClosedHandler = () => {
+		this.setState((prevState) => {
+			return { showSideDrawer: !prevState.showSideDrawer };
+		});
+	};
+
+	render() {
+		return (
+			<Aux>
+				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+				<SideDrawer
+					open={this.state.showSideDrawer}
+					closed={this.sideDrawerClosedHandler}
+				/>
+				<main className={styles.content}>{this.props.children}</main>
+			</Aux>
+		);
+	}
+}
+
+export default Layout;
