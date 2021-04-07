4a74c8d019afb45f25080b89b3bc544a4b0ab912
Gary Hake <Gary.Hake@waystar.com>
2021-04-07 19:43:33 -0400

    redux and ui state

--- 12-burger-builder-redux/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 12-burger-builder-redux/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
 	state = {
-		purchasable: false,
 		purchasing: false,
 		loading: false,
 		error: false,
@@ -34,16 +33,16 @@ class BurgerBuilder extends Component {
 	// 		});
 	// }
 
-	updatePurchaseState(ingredients) {
-		const sum = Object.keys(ingredients)
+	updatePurchaseState() {
+		const sum = Object.keys(this.props.ings)
 			.map((igKey) => {
-				return ingredients[igKey];
+				return this.props.ings[igKey];
 			})
 			.reduce((sum, el) => {
 				return sum + el;
 			}, 0);
 
-		this.setState({ purchasable: sum > 0 });
+		return sum > 0;
 	}
 
 	purchaseHandler = () => {
@@ -98,7 +97,7 @@ class BurgerBuilder extends Component {
 						ingredientAdded={this.props.onIngredientAdded}
 						ingredientRemoved={this.props.onIngredientRemoved}
 						disabled={disableInfo}
-						purchasable={this.state.purchasable}
+						purchasable={this.updatePurchaseState()}
 						ordered={this.purchaseHandler}
 						price={this.props.price}
 					/>
