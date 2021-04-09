c6108747a6cd384d46f4d29919377e6204425cb2
Gary Hake <Gary.Hake@waystar.com>
2021-04-09 07:45:50 -0400

    adding react-thunk

--- 14-burger-builder-redux-adv/src/index.js
+++ 14-burger-builder-redux-adv/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import reducer from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
 	<React.StrictMode>
