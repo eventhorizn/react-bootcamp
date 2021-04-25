e674a70388e84a1f7908df71bbe30e4e799d9792
Gary Hake <Gary.Hake@waystar.com>
2021-04-25 10:56:34 -0400

    using context api

--- 19-replace-redux-hooks/src/containers/Products.js
+++ 19-replace-redux-hooks/src/containers/Products.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from 'react';
+//import { useSelector } from 'react-redux';
 
 import ProductItem from '../components/Products/ProductItem';
+import { ProductsContext } from '../context/products';
 import './Products.css';
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
-  return (
-    <ul className="products-list">
-      {productList.map(prod => (
-        <ProductItem
-          key={prod.id}
-          id={prod.id}
-          title={prod.title}
-          description={prod.description}
-          isFav={prod.isFavorite}
-        />
-      ))}
-    </ul>
-  );
+const Products = (props) => {
+	const productList = useContext(ProductsContext).products;
+
+	//const productList = useSelector((state) => state.shop.products);
+	return (
+		<ul className="products-list">
+			{productList.map((prod) => (
+				<ProductItem
+					key={prod.id}
+					id={prod.id}
+					title={prod.title}
+					description={prod.description}
+					isFav={prod.isFavorite}
+				/>
+			))}
+		</ul>
+	);
 };
 
 export default Products;
--- 19-replace-redux-hooks/src/index.js
+++ 19-replace-redux-hooks/src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+//import productReducer from './store/reducers/products';
+import ProductsProvider from './context/products';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+// const rootReducer = combineReducers({
+// 	shop: productReducer,
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+	<ProductsProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</ProductsProvider>,
+	document.getElementById('root')
 );
